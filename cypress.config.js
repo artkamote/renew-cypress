93a0dc64ea6f14d4aed666fc3a1258760c31111d
jhayphie Sapitan <pablo.sapitan@live.com>
2022-07-25 23:40:58 +0800

    Update the config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
 const { defineConfig } = require('cypress')
-process.env.HOST='https://automationteststore.com/'
-process.env.DEMOHOST='https://demo.realworld.io/#/settings'
-process.env.API_URL = 'https://api.realworld.io/api/users/login'
-process.env.EMAIL = '<YOUR EMAIL>'
-process.env.PASSWORD = '<YOUR PASSWORD>'
+// process.env.HOST='https://automationteststore.com/'
+// process.env.DEMOHOST='https://demo.realworld.io/#/settings'
+// process.env.API_URL = 'https://api.realworld.io/api/users/login'
+// process.env.EMAIL = '<YOUR EMAIL>'
+// process.env.PASSWORD = '<YOUR PASSWORD>'
 
 module.exports = defineConfig({
   // setupNodeEvents can be defined in either
@@ -12,11 +12,11 @@ module.exports = defineConfig({
       defaultCommandTimeout: 30000,
       video: false,
     env: {
-      'host' : process.env.HOST,
-      'demohost' : process.env.DEMOHOST,
+      'host': process.env.HOST,
+      'demohost': process.env.DEMOHOST,
       'apiurl': process.env.API_URL,
       'email': process.env.EMAIL,
-      'password':process.env.PASSWORD
+      'password': process.env.PASSWORD
     },
         setupNodeEvents(on, config) {
       console.log(config) // see everything in here!
