9c33542d737aa961f4c6e96680f8e2fe45a9efc5
Art Cuaresma <artc@ArtKamote-Macbook.local>
2022-07-26 21:29:30 +0800

    comment process.env

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,10 @@
 const { defineConfig } = require('cypress')
- process.env.HOST='https://automationteststore.com/'
- process.env.WEBDRIVERUNI='http://www.webdriveruniversity.com/'
- process.env.DEMOHOST='https://demo.realworld.io/#'
- process.env.API_URL = 'https://api.realworld.io/api/users/login'
- process.env.EMAIL = 'sample1234@gmail.com'
- process.env.PASSWORD = 'Welcome12345'
+ //process.env.HOST='https://automationteststore.com/'
+ //process.env.WEBDRIVERUNI='http://www.webdriveruniversity.com/'
+ //process.env.DEMOHOST='https://demo.realworld.io/#'
+ //process.env.API_URL = 'https://api.realworld.io/api/users/login'
+ //process.env.EMAIL = 'sample1234@gmail.com'
+ //process.env.PASSWORD = 'Welcome12345'
 
 module.exports = defineConfig({
   // setupNodeEvents can be defined in either
