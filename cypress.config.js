7bc1d457cc8f0cde3b50b127d32b9c7f1186e275
jhayphie Sapitan <pablo.sapitan@live.com>
2022-07-27 00:26:11 +0800

    Add allure reporting on cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,19 @@
 const { defineConfig } = require('cypress')
-// process.env.HOST='https://automationteststore.com/'
-// process.env.WEBDRIVERUNI='http://www.webdriveruniversity.com/'
-// process.env.DEMOHOST='https://demo.realworld.io/#'
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
+// process.env.HOST = 'https://automationteststore.com/'
+// process.env.WEBDRIVERUNI = 'http://www.webdriveruniversity.com/'
+// process.env.DEMOHOST = 'https://demo.realworld.io/#'
 // process.env.API_URL = 'https://api.realworld.io/api/users/login'
-// process.env.EMAIL = <YOUR EMAIL>
-// process.env.PASSWORD = <YOUR PASSWORD>
+// process.env.EMAIL = 'sample1234@gmail.com'
+// process.env.PASSWORD = 'Welcome12345'
 
 module.exports = defineConfig({
   // setupNodeEvents can be defined in either
   // the e2e or component configuration
   e2e: {
-      defaultCommandTimeout: 30000,
-      video: false,
+    defaultCommandTimeout: 30000,
+    video: false,
     env: {
       'host': process.env.HOST,
       'demohost': process.env.DEMOHOST,
@@ -20,12 +22,12 @@ module.exports = defineConfig({
       'email': process.env.EMAIL,
       'password': process.env.PASSWORD
     },
-        setupNodeEvents(on, config) {
+    setupNodeEvents(on, config) {
       console.log(config) // see everything in here!
-      
-      
+      allureWriter(on, config);
+
       // IMPORTANT return the updated config object
       return config
     }
-  }
+  },
 })
