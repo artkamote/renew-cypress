1959a3758dba8b34f6fd54e0123d477adc164895
pabs <juanpablosapitan@pabss-MacBook-Air.local>
2022-07-28 20:11:30 -0700

    remove the cypress-snapshot-images

--- cypress.config.js
+++ cypress.config.js
@@ -2,13 +2,13 @@ const { defineConfig } = require('cypress')
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 const { initPlugin } = require('cypress-plugin-snapshots/plugin');
 
-  process.env.HOST = 'https://automationteststore.com/'
-  process.env.WEBDRIVERUNI = 'http://www.webdriveruniversity.com/'
-  process.env.DEMOHOST = 'https://demo.realworld.io/#'
-  process.env.API_URL = 'https://api.realworld.io/api/users/login'
-  process.env.EMAIL = 'sample1234@gmail.com'
-  process.env.PASSWORD = 'Welcome12345'
-  process.env.THRESHOLD = 0.01
+  // process.env.HOST = 'https://automationteststore.com/'
+  // process.env.WEBDRIVERUNI = 'http://www.webdriveruniversity.com/'
+  // process.env.DEMOHOST = 'https://demo.realworld.io/#'
+  // process.env.API_URL = 'https://api.realworld.io/api/users/login'
+  // process.env.EMAIL = 'sample1234@gmail.com'
+  // process.env.PASSWORD = 'Welcome12345'
+  // process.env.THRESHOLD = 0.01
 
 module.exports = defineConfig({
   // setupNodeEvents can be defined in either
@@ -16,10 +16,6 @@ module.exports = defineConfig({
   e2e: {
     defaultCommandTimeout: 30000,
     video: false,
-    excludeSpecPattern : [
-      "**/__snapshots__/*",
-      "**/__image_snapshots__/*"
-    ],
 
     env: {
       'host': process.env.HOST,
@@ -27,12 +23,7 @@ module.exports = defineConfig({
       'apiurl': process.env.API_URL,
       'webdriveruniurl': process.env.WEBDRIVERUNI,
       'email': process.env.EMAIL,
-      'password': process.env.PASSWORD,
-      'cypress-plugin-snapshots': {
-        "imageConfig": {
-          "threshold": process.env.THRESHOLD
-        }
-      }
+      'password': process.env.PASSWORD
     },
     setupNodeEvents(on, config) {
       console.log(config) // see everything in here!
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,7 +16,6 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import '@shelex/cypress-allure-plugin';
-import 'cypress-plugin-snapshots/commands';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
