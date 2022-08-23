d5160bcb599ca9a9242386e6abf2a07a82690425
jhayphie Sapitan <pablosapitan28@gmail.com>
2022-08-24 00:21:31 +0800

    Add API Testing

---
+++ cypress/e2e/examples/integration/get-requests.cy/get-requests.cy.js
@@ -0,0 +1,25 @@
+
+
+describe('Get Request', () =>{
+    var result;
+    it('Validate status code of the /articles api', () => {
+        result = cy.request('https://api.realworld.io/api/articles?limit=10&offset=0&tag=welcome')
+        result.its("status").should("equal", 200);
+    })
+
+    it('Validate /articles api contains the correct keys and values', () => {
+       cy.request({
+            method: "GET",
+            url: "https://api.realworld.io/api/articles?limit=10&offset=0&tag=welcome",
+            headers: {
+                accept: "application/json"
+            }
+       }).then(response => {
+            let body = JSON.parse(JSON.stringify(response.body));
+            cy.log(body.articles[0].author.username);
+
+            expect(body.articles[0]).has.property("title", "Welcome to RealWorld project");
+            expect(body.articles[0].author).has.property("username", "Gerome");
+       })
+    })
+})
