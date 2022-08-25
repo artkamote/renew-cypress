25bb50583babf93ed9c35c042ef976a3f717438e
jhayphie Sapitan <pablosapitan28@gmail.com>
2022-08-26 00:24:41 +0800

    Add more API testing, Add Post and Delete articles

---
+++ cypress/e2e/examples/integration/get-requests.cy/post-request.cy.js
@@ -0,0 +1,34 @@
+describe('Post Request', () => {
+    it('post Request', () => {
+        cy.getAndSetToken(Cypress.env('apiurl'), Cypress.env('email'), Cypress.env('password')).then((res) => {
+            cy.request({
+                method: "POST",
+                url: Cypress.env('api_realworld') + 'articles',
+                headers: {
+                    accept: "application/json",
+                    authorization: "Token" + ' ' + res.body.user.token
+                },
+                body: {
+                    "article": {
+                        "title": "test article of post 5",
+                        "description": "testing only",
+                        "body": "this is a test article only",
+                        "tagList": []
+                    }
+                }
+            }).then(response => {
+                expect(response.body.article.title).to.eql("test article of post 5");
+                cy.request({
+                    method: "DELETE",
+                    url: Cypress.env('api_realworld') + 'articles/test-article-of-post-5-67598',
+                    headers: {
+                        accept: "application/json",
+                        authorization: "Token" + ' ' + res.body.user.token
+                    }
+                }).then(response => {
+                    expect(response.status).to.eql(200);
+                })
+            })
+        })
+    })
+})
